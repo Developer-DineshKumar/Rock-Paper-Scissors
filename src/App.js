2f027a66f2702510797fefbf6c069d86673922e5
Developer-DineshKumar <kumar05dinesh@gmail.com>
2023-02-24 07:37:59 +0000

    Rock-Paper-Scissors

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/extensions
+import PlayingView from './component/PlayingView'
+import './App.css'
+/*
+
+const choicesList = [
+  {
+    id: 'ROCK',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+  },
+  {
+    id: 'SCISSORS',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+  },
+  {
+    id: 'PAPER',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+  },
+]
+*/
+
+const App = () => <PlayingView />
+
+export default App
